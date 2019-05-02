463ea3ddfa7421d6c6173e664b449acd5372f236
Neeraj Negi <neeraj33negi@gmail.com>
2019-05-02 15:08:01 +0530

    Change indetation to spaces in JS Exercises

--- js-jquery/JS_exercises/1_compareAge.js
+++ js-jquery/JS_exercises/1_compareAge.js
@@ -1,13 +1,13 @@
 class User{
-	constructor(name, age){
-		this.name = name;
-		this.age = age;
-	}
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+  }
 
-	compareAge(user){
-		var returnStr = " is older than ";
-		return ( this.age > user.age ) ? this.name + returnStr + user.name : user.name + returnStr + this.name;
-	};
+  compareAge(user){
+    var returnStr = " is older than ";
+    return ( this.age > user.age ) ? this.name + returnStr + user.name : user.name + returnStr + this.name;
+  };
 }
 
 var user1 = new User("neeraj", 10);
