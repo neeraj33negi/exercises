8d8f2cf22eba7591800c282abcd8332f5c21c5ff
Neeraj Negi <neeraj33negi@gmail.com>
2019-05-14 17:15:49 +0530

    Update solution of first exercise - use default hash value, remove unless-else, use space after returning else, use  instead of

--- ruby/occurrence_hash.rb
+++ ruby/occurrence_hash.rb
@@ -1,20 +1,17 @@
 class OccurrenceHash
   attr_accessor :string
   def initialize arg
-    @string = arg.reduce("") {|initial, curr| initial + curr}
+    @string = arg.join('')
   end
   def count_occurrences
-    hash = {}
+    hash = Hash.new(0)
     for i in 0..@string.length
       if /[a-zA-Z]/.match(@string[i])
-        unless hash.has_key? (@string[i])
-          hash[@string[i]] = 1
-        else
           hash[@string[i]] = hash[@string[i]] + 1
-        end
       end
     end
     hash
+
   end
 end
 
