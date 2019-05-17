ecd091116c3592a51681534a306b584ddf38383b
Neeraj Negi <neeraj33negi@gmail.com>
2019-05-17 15:10:55 +0530

    Add solution to ninth exercise(array to hash)

---
+++ ruby/array_hash.rb
@@ -0,0 +1,17 @@
+class Array
+  def to_hash
+    hash = {}
+    self.each do |variable|
+      if hash.has_key?(variable.to_s.length)
+        hash[variable.to_s.length] << variable
+      else
+        hash[variable.to_s.length] = [variable.gsub(/["''\[\]]/, "")]
+      end
+    end
+    hash.sort.to_h
+  end
+
+end
+
+arr = ARGV[0].gsub(/[\[\]"']/,"").split(',')
+puts arr.to_hash
