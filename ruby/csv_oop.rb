cba55b55813ef44989700a0fd195147b06b7318c
Neeraj Negi <neeraj33negi@gmail.com>
2019-05-29 15:42:55 +0530

    Change solution of ex-23(csv to plain file)

--- ruby/csv_oop.rb
+++ ruby/csv_oop.rb
@@ -1,62 +1,83 @@
 require 'csv'
 
 class Employee
-  @@designation_count = {Designer: 0, Tester: 0, Developer: 0}
-  @@employees = []
   attr_accessor :name, :id, :designation
-  def initialize(name:, id:, designation:)
-    case designation
-    when "Developer"
-      @@designation_count[:Developer] += 1
-    when "Designer"
-      @@designation_count[:Designer] += 1
-    when "Tester"
-      @@designation_count[:Tester] += 1
-    else
-      raise ArgumentError, "Invalid designation"
-    end
-    @name, @id, @designation = name, id.to_i, designation
-  end
-
-  class << self
-    def read_from_csv(path)
-      csv_content = CSV.read(path)
-      csv_content.shift
-      @@employees = csv_content.map do |current_line|
-        Employee.new(name:current_line[0], id:current_line[1], designation:current_line[2].strip)
-      end
-    end
-
-    def write_to_file(filename)
-      hash = {}
-      @@employees.each do |emp|
-        if @@designation_count[emp.designation.to_sym] > 1
-          if hash.has_key? (emp.designation + 's')
-            hash[emp.designation + 's'] << "#{emp.name} (Empid:#{emp.id})"
-          else
-            hash[emp.designation + 's'] = ["#{emp.name} (Empid:#{emp.id})"]
-          end
-        else
-          if hash.has_key? (emp.designation)
-            hash[emp.designation] << "#{emp.name} (Empid:#{emp.id})"
-          else
-            hash[emp.designation] = ["#{emp.name} (Empid:#{emp.id})"]
-          end
-        end
-      end
-      hash = hash.sort.to_h
-      file = File.new(filename, "w+")
-      hash.each do |key, value|
-        file << (key.strip + "\n")
-        value.each do |emp|
-          file << (emp + "\n")
-        end
-      end
-      file.close
-    end
-
+  def initialize(name:, id:)
+    @name, @id = name, id.to_i
   end
 end
 
-Employee.read_from_csv("employees.csv")
-Employee.write_to_file("aa.txt")
+class Developer < Employee
+  def initialize(name:, id:)
+    super
+    @designation = "Developer"
+  end
+end
+
+class Designer < Employee
+  def initialize(name:, id:)
+    super
+    @designation = "Designer"
+  end
+end
+
+class Tester < Employee
+  def initialize(name:, id:)
+    super
+    @designation = "Tester"
+  end
+end
+
+class Solution
+  def initialize
+    @employees = []
+  end
+
+  def read_csv_in_hash(path)
+    hash = Hash.new
+    csv_content = CSV.read(path)
+    csv_content.shift
+    csv_content.each do |emp|
+      case emp[2].strip
+      when "Tester"
+        @employees << Tester.new(name:emp[0], id:emp[1])
+        designation = "Tester"
+      when "Developer"
+        @employees << Developer.new(name:emp[0], id:emp[1])
+        designation = "Developer"
+      when "Designer"
+        @employees << Designer.new(name:emp[0], id:emp[1])
+        designation = "Designer"
+      end
+    end
+    @employees.each do |emp|
+      if hash.has_key? (emp.class.to_s)
+        hash[emp.class.to_s] << "#{emp.name} (Empid:#{emp.id})"
+      else
+        hash[emp.class.to_s] = ["#{emp.name} (Empid:#{emp.id})"]
+      end
+    end
+    hash["Testers"] = hash.delete("Tester") if hash["Tester"].length > 1
+    hash["Developers"] = hash.delete("Developer") if hash["Developer"].length > 1
+    hash["Designers"] = hash.delete("Designer") if hash["Designer"].length > 1
+    hash.sort.to_h
+  end
+
+  def write_hash_to_file(filename, hash)
+    file = File.new(filename, "w+")
+    hash.each do |key, value|
+      file << (key.strip + "\n")
+      value.each do |emp|
+        file << (emp + "\n")
+      end
+    end
+    file.close
+  end
+
+  def csv_to_plain_file(source_csv, target_file)
+    self.write_hash_to_file(target_file, read_csv_in_hash(source_csv) )
+  end
+
+end
+
+Solution.new.csv_to_plain_file("employees.csv", "solve.txt")
